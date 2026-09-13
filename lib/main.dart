df0e1600a1a6e358dec8a4f07c98e9df1a8f4a0c
Ayman Ja. <95094618+aymanjameel60@users.noreply.github.com>
2026-09-13 04:39:38 +0300

    Clean Flutter analyzer imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
